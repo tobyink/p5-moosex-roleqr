cfb5479c1226b0d61b723a109964cb4cdd58b5c9
Toby Inkster <mail@tobyinkster.co.uk>
2012-10-17 13:42:40 +0100

    The great new MooseX::RoleQR. So far, just a quick test case

---
+++ lib/MooseX/RoleQR.pm
@@ -0,0 +1,55 @@
+package MooseX::RoleQR;
+
+use 5.010;
+use constant { false => 0, true => 1 };
+use strict;
+use warnings;
+use utf8;
+
+BEGIN {
+	$MooseX::RoleQR::AUTHORITY = 'cpan:TOBYINK';
+	$MooseX::RoleQR::VERSION   = '0.001';
+}
+
+
+
+
+# Your code goes here
+
+true;
+
+__END__
+
+=head1 NAME
+
+MooseX::RoleQR - allow "before qr{...} => sub {...};" in roles
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=MooseX-RoleQR>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
