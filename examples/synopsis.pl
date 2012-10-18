eabcb2bf65bf1dbc638a3b1abd5ae7011c6066b0
Toby Inkster <mail@tobyinkster.co.uk>
2012-10-18 13:36:21 +0100

    fix indents

--- examples/synopsis.pl
+++ examples/synopsis.pl
@@ -2,20 +2,20 @@ use strict;
 use warnings;
 
 {
-   package Local::Role;
-   use MooseX::RoleQR;
-   after qr{^gr} => sub {
-      print " World\n";
-   };
+	package Local::Role;
+	use MooseX::RoleQR;
+	after qr{^gr} => sub {
+		print " World\n";
+	};
 }
  
 {
-   package Local::Class;
-   use Moose;
-   with qw( Local::Role );
-   sub greet {
-      print "Hello";
-   }
+	package Local::Class;
+	use Moose;
+	with qw( Local::Role );
+	sub greet {
+		print "Hello";
+	}
 }
  
 Local::Class->new->greet; # prints "Hello World\n"
